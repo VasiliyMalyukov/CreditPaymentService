39ce11c37c0ef81018111302547534b09200f2a5
Wovasya <v37444170895@gmail.com>
2023-01-19 17:24:41 +0400

    renaming a variable

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,7 +1,7 @@
 public class CreditPaymentService {
-    public double calculate(int credit, double percent, int month){
-        double percent_month = percent / 12;
-        double coefficient = (percent_month * Math.pow((1 + percent_month), month)) / ((Math.pow((1 + percent_month), month)) -1);
+    public double calculate(int credit, double percent, int month) {
+        double percentMonth = percent / 12;
+        double coefficient = (percentMonth * Math.pow((1 + percentMonth), month)) / ((Math.pow((1 + percentMonth), month)) - 1);
         double payment = Math.round(credit * coefficient);
         return payment;
     }
